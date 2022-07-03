4d2bf9087152e56309987f8e58d181ac428a1bc2
Steven Creekmore <home@Vishnu.local>
2022-07-02 23:49:12 -0600

    Design Add idea form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,39 @@
 
           <main class="container mx-auto max-w-custom flex">
               <div class="w-70 mr-5">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum voluptate culpa veniam quaerat, quia nihil blanditiis quos incidunt necessitatibus placeat error praesentium, eos iusto! Quas fugiat atque harum sapiente nobis incidunt fugit. Optio, laudantium. Quod maiores suscipit modi unde dignissimos enim voluptatem molestiae debitis, minus odit tenetur sit hic dicta ipsum fuga laudantium cupiditate soluta, nemo, corrupti accusantium quo delectus animi. Magni ipsa vel, distinctio provident saepe ipsam officia debitis, fuga eaque quaerat corrupti magnam ad culpa laboriosam ut iusto id aliquam. Fuga consectetur porro modi ex earum iusto sed eaque natus, eos, dolorem optio. Laudantium ullam assumenda totam iusto.
+                  <div class="bg-white border-2 border-blue rounded-xl mt-16">
+                     <div class="text-center px-6 py-2 pt-6">
+                         <h3 class="font-semibold text-base">Add an idea</h3>
+                         <p class="text-xs mt-4">Let us know what you would like and we'll take a look over!</p>
+                     </div>
+                    <form action="#" method="POST" class="space-y-4 px-4 py-6">
+                         <div>
+                            <input type="text" class="text-sm w-full border-none bg-gray-100 rounded-xl placeholder-gray-900 px-4 py-2" placeholder="Your idea">
+                         </div>
+                         <div>
+                            <select name="category_add" id="category_add" class="bg-gray-100 text-sm w-full rounded-xl border-none px-4 py2">
+                                <option value="Category One">Category One</option>
+                                <option value="Category Two">Category Two</option>
+                                <option value="Category Three">Category Three</option>
+                            </select>
+                         </div>
+                         <div>
+                             <textarea name="idea" id="idea" cols="30" rows="4" class="w-full border-none bg-gray-100 rounded-xl placeholder-gray-900 text-sm px-4 py-2" placeholder="Describe your idea"></textarea>
+                         </div>
+                         <div class="flex items-center justify-between space-x-3">
+                             <button type="button" class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
+                                <svg class="text-gray-500 w-4 transform -rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                                </svg> 
+                                <span class="ml-1">Attach</span>
+                             </button>
+                             <button type="submit" class="flex items-center justify-center w-1/2 h-11 text-xs bg-blue font-semibold rounded-xl border border-blue hover:bg-blue-hover text-white transition duration-150 ease-in px-6 py-3">
+                                
+                                <span class="ml-1">Submit</span>
+                             </button>
+                         </div>
+                    </form>
+                  </div>
               </div>
               <div class="w-175">
                   <nav class="flex items-center justify-between text-xs">
@@ -66,6 +98,7 @@
                       {{ $slot }}
                   </div>
               </div>
+              
           </main>
     
     </body>
